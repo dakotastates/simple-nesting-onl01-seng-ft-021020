5f82da9743d51a5199e7e93f88071829dd669b07
Dakota States <dakota.states@gmail.com>
2020-02-17 03:22:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,6 +85,7 @@ def adding_matz
 
 
     }
+    
    programmer_hash[:yukihiro_matsumoto] = {
     known_for: "Ruby",
     languages:
@@ -132,5 +133,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+
 end
