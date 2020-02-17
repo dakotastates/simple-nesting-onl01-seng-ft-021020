a4dd52c863a0dcd1d2fb0c8020b0ab61ad87f0fb
Dakota States <dakota.states@gmail.com>
2020-02-17 03:24:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,11 +86,7 @@ def adding_matz
 
     }
     
-   programmer_hash[:yukihiro_matsumoto] = {
-    known_for: "Ruby",
-    languages:
-      ["LISP", "C"]
-    }
+
     
     
 end
@@ -112,7 +108,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-
+     alans_new_info = "GUI"
+    programmer_hash[:alan_kay][:known_for] = "GUI"
      
 end
 
