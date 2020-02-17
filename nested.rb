296d0c407529623ca8c91c090bc4c7f1f6f4e549
Dakota States <dakota.states@gmail.com>
2020-02-17 02:30:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     return programmer_hash[:dennis_ritchies][:languages].pair
+     return programmer_hash[:dennis_ritchies][:languages].join
 end
 
 def adding_matz
